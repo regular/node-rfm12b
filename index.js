61bda6fdc369a8da1e82b670e662a5bad6264e6e
Jan Bölsche <regular.gonzales@googlemail.com>
2018-06-28 10:33:01 +0000

    Add getter/setters

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const fs = require('fs')
 const ioctl = require('ioctl')
+const camelCase = require('camel-case')
 const {rfm12b_ioctl} = require('./rfm12b_ioctl');
 
-const rfm12_fd = fs.openSync('/dev/rfm12b.0.1', 'rs+')
-console.error('fd', rfm12_fd)
+const _accessors = {
+  GROUP_ID: [getInt, setInt],
+  BAND_ID: [getInt, setInt],
+  BIT_RATE: [getInt, setInt],
+  JEE_ID: [getInt, setInt],
+  JEEMODE_AUTOACK: [getInt, setInt]
+}
 
-const band_id = getInt(rfm12_fd, rfm12b_ioctl.GET_BAND_ID)
-const bit_rate = getInt(rfm12_fd, rfm12b_ioctl.GET_BIT_RATE)
+module.exports = function(opts) {
+  opts = opts || {}
+  const rfm12_fd = fs.openSync(opts.device_path || '/dev/rfm12b.0.1', 'rs+')
 
-console.error('band_id:', band_id, 'bit_rate', bit_rate)
+  const radio = Object.keys(_accessors).reduce( ((acc, name) =>
+    Object.assign(acc, makeGetterSetter(name, rfm12_fd))
+  ), {})
+  radio.close = function() {
+    fs.closeSync(rfm12_fd)
+  }
+  return radio
+}
 
-fs.closeSync(rfm12_fd)
+function makeGetterSetter(name, fd) {
+  const [get, set] = _accessors[name]
+  const getter_ioctl_name = 'GET_' + name
+  const setter_ioctl_name = 'SET_' + name
+  const getter_ioctl = rfm12b_ioctl[getter_ioctl_name]
+  const setter_ioctl = rfm12b_ioctl[setter_ioctl_name]
+  const getterName = camelCase(getter_ioctl_name)
+  const setterName = camelCase(setter_ioctl_name)
+
+  return {
+    [getterName]: function() {
+      return get(fd, getter_ioctl)
+    },
+    [setterName]: function(value) {
+      return set(fd, setter_ioctl, value)
+    }
+  }
+}
 
 function getInt(fd, code) {
   const b = new Buffer(4);
@@ -18,3 +49,11 @@ function getInt(fd, code) {
   if (ret) throw new Error('ioctl failed');
   return b.readInt32LE(0);
 }
+
+function setInt(fd, code, value) {
+  const b = new Buffer(4);
+  b.writeInt32LE(value, 0);
+  const ret = ioctl(fd, code, b);
+  if (ret) throw new Error('ioctl failed');
+}
+
---
+++ test.js
@@ -0,0 +1,14 @@
+const radio = require('./')()
+
+const JEE_ID = 13
+const GROUP_ID =19
+
+radio.setJeeId(JEE_ID)
+radio.setGroupId(GROUP_ID)
+radio.setJeemodeAutoack(1)
+
+Object.keys(radio).forEach(k => {
+  if (k.slice(0,3) == 'get') {
+    console.log(k, radio[k]())
+  }
+})
